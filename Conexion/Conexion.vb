2068fa0ba687d7a6d65a0b817d04503ff23ef67b
Martin-Fuentes <martinfuentes012@gmail.com>
2022-11-07 17:15:28 -0500

    Agregar archivos de proyecto.

---
+++ Conexion/Conexion.vb
@@ -0,0 +1,11 @@
+﻿Imports MySql.Data.MySqlClient
+Public Class conexion
+    Private cnx As MySqlConnection
+    Public Function getConexion()
+        If cnx Is Nothing Then
+            cnx = New MySqlConnection("Server=localhost; UserID=root; Password=; Database=bdcompras;")
+            cnx.Open()
+        End If
+        Return cnx
+    End Function
+End Class
